13b83ea549059b603dbc4be1f7b99f84740c81fc
fabiodesilva01 <fabiodesilva01@gmail.com>
2026-04-19 10:46:07 -0300

    adicionado tratamento de erros de autenticação

--- src/app/interceptors/controles.interceptor.ts
+++ src/app/interceptors/controles.interceptor.ts
@@ -1,13 +1,14 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { LoadingService } from '../services/loading.service';
-import { finalize } from 'rxjs';
+import { catchError, finalize, switchMap, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 export const controlesInterceptor: HttpInterceptorFn = (req, next) => {
   const loading = inject(LoadingService);
   const authService = inject(AuthService);
-
+  const router = inject(Router);
   const token = authService.token();
 
   let authReq = req;
@@ -19,12 +20,40 @@ export const controlesInterceptor: HttpInterceptorFn = (req, next) => {
         Authorization: `Bearer ${token}` // padrão JWT
       }
     });
-  }
+  } 
 
   // Exibe o loading antes de enviar a requisição
   loading.show();
 
+  // Envia a requisição e trata erros de autenticação
   return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+
+      if (error.status === 401) {
+        // tenta renovar
+        return authService.refreshToken().pipe(
+          switchMap((newToken: string) => {
+            // salva novo token
+            authService.setToken(newToken);
+
+            // refaz a request original com novo token
+            const retryReq = req.clone({
+              setHeaders: { Authorization: `Bearer ${newToken}` }
+            });
+
+            return next(retryReq);
+          }),
+          catchError(() => {
+            // refresh falhou → logout
+            authService.logout();
+            router.navigate(['/login']);
+            return throwError(() => error);
+          })
+        );
+      }
+      return throwError(() => error);
+    }),
     finalize(() => loading.hide())
   );
+  
 };
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, signal, WritableSignal } from '@angular/core';
+import { delay, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,9 +7,31 @@ import { Injectable, signal, WritableSignal } from '@angular/core';
 export class AuthService {
 
   readonly isAuthenticated: WritableSignal<boolean> = signal(true);
-  readonly token: WritableSignal<string> = signal('');
-
+  readonly token: WritableSignal<string> = signal('token-inicial');
 
   constructor() { }
+
+  refreshToken() {
+    return of('novo-token').pipe(
+      delay(500), // simula requisição
+      tap(token => this.setToken(token))
+    );
+  }
+
+  setToken(token: string) {
+    this.token.set(token);
+    this.isAuthenticated.set(true);
+  }
+
+  logout() {
+    this.token.set('');
+    this.isAuthenticated.set(false);
+  }
+
+  login(username: string, password: string) {
+    // Simula uma autenticação bem-sucedida
+    this.token.set('token-de-autenticacao');
+    this.isAuthenticated.set(true);
+  }
 
 }
